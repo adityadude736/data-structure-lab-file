48083c8604f67405eb51430d352172f7b4da3a91
Aditya Singh <adityasingh15934@gmail.com>
2025-11-26 14:50:02 +0530

    Implement BrowserHistory with back and forward navigation

--- BrowserHistory.java
+++ BrowserHistory.java
@@ -1,1 +1,44 @@
+import java.util.Stack;
 
+public class BrowserHistory {
+    Stack<String> back = new Stack<>();
+    Stack<String> forward = new Stack<>();
+    String currentPage = "Home";
+
+    void visit(String url) {
+        back.push(currentPage);
+        currentPage = url;
+        forward.clear();
+        System.out.println("Visited: " + currentPage);
+    }
+
+    void goBack() {
+        if (!back.isEmpty()) {
+            forward.push(currentPage);
+            currentPage = back.pop();
+            System.out.println("Back to: " + currentPage);
+        } else {
+            System.out.println("No pages in back history");
+        }
+    }
+
+    void goForward() {
+        if (!forward.isEmpty()) {
+            back.push(currentPage);
+            currentPage = forward.pop();
+            System.out.println("Forward to: " + currentPage);
+        } else {
+            System.out.println("No pages in forward history");
+        }
+    }
+
+    public static void main(String[] args) {
+        BrowserHistory bh = new BrowserHistory();
+        bh.visit("google.com");
+        bh.visit("youtube.com");
+        bh.visit("github.com");
+        bh.goBack();
+        bh.goBack();
+        bh.goForward();
+    }
+}
